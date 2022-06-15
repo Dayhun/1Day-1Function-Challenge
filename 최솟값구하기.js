10d9bdb24ca9cf2e6ad2fb7c481927afe0d12506
Dayhun <lanna05@naver.com>
2022-06-15 19:18:26 +0900

    최솟값 구하기

---
+++ 최솟값구하기.js
@@ -0,0 +1,12 @@
+function min(array) {
+    let output = array[0];
+    for (const item of array) {
+        if (output > item) {
+            output = item;
+        }
+    }
+    return output;
+}
+
+const testarray = [35, 5, 45, 27, 30];
+console.log(`${testarray}의 최솟값은 ${min(testarray)}입니다.`);
