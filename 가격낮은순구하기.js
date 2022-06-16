af3292b96bdf4304369340fe4b7ab6590fd5f9ef
Dayhun <lanna05@naver.com>
2022-06-16 12:00:15 +0900

    0615. 가격낮은순 구하기

---
+++ 가격낮은순구하기.js
@@ -0,0 +1,27 @@
+// product 할당부분은 복붙해놓고 시작~ 
+let products = [
+    { id: 0, price: 70000, title: 'Blossom Dress' },
+    { id: 1, price: 32000, title: 'Check Shirt' },
+    { id: 2, price: 15000, title: 'Black Tie' },
+    { id: 3, price: 67000, title: 'Springfield Shirt' },
+    { id: 4, price: 82000, title: 'Red Mini Dress' },
+];
+
+// 가격 낮은 순
+let lowToHigh = function () {
+    products.sort(function (a, b) {
+        return a.price - b.price;
+    });
+    console.log(products);
+};
+lowToHigh();
+
+// 가격 높은 순
+let highToLow = function () {
+    products.sort(function (a, b) {
+        return b.price - a.price;
+    });
+    console.log(products);
+};
+highToLow();
+
