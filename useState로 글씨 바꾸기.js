3aeebb1f55a4a04e47d692623c57307668166c19
Dayhun <lanna05@naver.com>
2022-06-30 01:17:06 +0900

    0629. useState로 글씨 바꾸기

--- useState로 글씨 바꾸기.js
+++ useState로 글씨 바꾸기.js
@@ -1,37 +1,33 @@
 import { useState } from 'react'
 
 function Hello(props) {
-    const [name, setName] = useState("태태룽")
-    const [age, setAge] = useState(props.age)
-    let msg = age > 19 ? "성인" : "미자"
-    let color = "green"
+    const [name, setName] = useState("김씨");
+    const [age, setAge] = useState(props.age);
+    let msg = age > 19 ? "성인" : "미성년자";
+    let color = "green";
 
     function changeColor() {
-        color = color === "green" ? "black" : "green"
-        document.querySelector('#color').textContent = color
+        color = color === "green" ? "black" : "green";
+        document.querySelector('#color').textContent = color;
     }
-
     return (
         <>
-            <h1>이름을 바꿔줘</h1>
-            <h3>{name} {age}세 {msg}</h3>
+            <h1>useState로 이름 바꾸기</h1>
+            <h3>{name} {age}세 {msg}입니다.</h3>
             <p id='color'>white</p>
             <button onClick={() => {
-                setName(name === "태태룽" ? "그린티" : "태태룽")
-                setAge(age + 1)
-            }}>
-                changeAgeName
-            </button>
-            <button onClick={changeColor}>changeColor</button>
+                setName(name === "김씨" ? "이씨" : "김씨");
+                setAge(age + 1);
+            }}>이름바꾸기</button>
+            <button onClick={changeColor}>색깔바꾸기</button>
         </>
     )
-
 }
 
-function App() {
+export default function App() {
     return (
-        <Hello age={16} />
+        <div>
+            <Hello age={16} />
+        </div>
     )
 }
-
-export default App
